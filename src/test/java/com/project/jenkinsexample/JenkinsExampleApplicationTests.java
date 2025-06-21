a97f1445f58189ab3c393739feb4cfb4b3ea7f78
Priyanshu <Priyanshubaral9562@gmail.com>
2025-06-21 12:40:55 +0530

    first commit

---
+++ src/test/java/com/project/jenkinsexample/JenkinsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jenkinsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
